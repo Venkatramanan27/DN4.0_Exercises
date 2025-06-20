82dac3febf8b42f05901d8550a0bdb50642715d4
Venkatramanan27 <venkatramanan2765@gmail.com>
2025-06-20 21:57:57 +0530

    Completed

--- Week_1/Design pattern and princple/FactoryMethodPatternExample/Code/src/Main.java
+++ Week_1/Design pattern and princple/FactoryMethodPatternExample/Code/src/Main.java
@@ -3,11 +3,9 @@ public class Main {
         DocumentFactory wordFactory = new WordDocumentFactory();
         Document wordDoc = wordFactory.createDocument();
         wordDoc.open();
-
         DocumentFactory pdfFactory = new PdfDocumentFactory();
         Document pdfDoc = pdfFactory.createDocument();
         pdfDoc.open();
-
         DocumentFactory excelFactory = new ExcelDocumentFactory();
         Document excelDoc = excelFactory.createDocument();
         excelDoc.open();
